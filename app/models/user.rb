c1be1fcbc8ab7e354bc576b01cc58b25aa8416f9
Rails Udemy Course <cesbecer@gmail.com>
2016-03-22 22:50:12 +0000

    Add user validations from a branch user-validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, length: { minimum: 3, maximun: 25} 
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 } 
+
+  # Test at http://rubular.com/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 end
