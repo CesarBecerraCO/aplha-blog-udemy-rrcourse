708d673c2fdabeeadde150b99213b575c188cdca
Rails Udemy Course <cesbecer@gmail.com>
2016-03-23 15:34:08 +0000

    Show and gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active_class(link_path)
     current_page?(link_path) ? "active" : ""
   end
+  
+  #Ver llamado desde .../users/show.html.erb
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    sz = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{sz}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
 end
