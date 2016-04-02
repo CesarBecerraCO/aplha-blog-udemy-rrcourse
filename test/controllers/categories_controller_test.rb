554a203acaa0e1435161623517e3a377fc49f69b
Rails Udemy Course <cesbecer@gmail.com>
2016-04-02 21:04:57 +0000

    Added categories edit option

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,6 +5,9 @@ class CategoriesControllerTest < ActionController::TestCase
   #Requerido para crear un objeto category named "sports"
   def setup
     @category = Category.create(name: "sports")
+    
+    #Requerido para verificar un usuario admin
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
   
   test "should get categories index" do
@@ -13,6 +16,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @user.id  #Create a session by the user admin id simulated
     get :new
     assert_response :success
   end
@@ -21,4 +25,12 @@ class CategoriesControllerTest < ActionController::TestCase
     get(:show, {'id' => @category.id})
     assert_response :success
   end
+  
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
+  end
+
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+  
   test "get new category form and create category" do
+    #This requiere the test_helper.rb method
+    sign_in_as(@user, "password")
+    
     get new_category_path
-      assert_template 'categories/new'
-      assert_difference 'Category.count', 1 do
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
       post_via_redirect categories_path, category: {name: "sports"}
     end
     assert_template 'categories/index'
@@ -12,6 +20,9 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission results in failure" do
+    #This requiere the test_helper.rb method
+    sign_in_as(@user, "password")
+    
     get new_category_path
       assert_template 'categories/new'
       assert_no_difference 'Category.count' do
